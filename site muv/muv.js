9d95f117db470a037627772c4fecc5cd9461fe8c
Lucas-Braga08 <lucasbsl2008@gmail.com>
2026-03-12 00:07:03 -0300

    Add files via upload

---
+++ site muv/muv.js
@@ -0,0 +1,29 @@
+function trocar(){
+
+alert("Você pode usar seus pontos para pedir comida grátis dentro do app!");
+
+}
+
+function home(){
+
+alert("Página inicial");
+
+}
+
+function pedidos(){
+
+alert("Aqui aparecerão seus pedidos.");
+
+}
+
+function pontos(){
+
+alert("Você possui 120 Muv Points!");
+
+}
+
+function perfil(){
+
+alert("Área do perfil do usuário.");
+
+}
